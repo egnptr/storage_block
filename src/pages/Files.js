f7d8439d7ae91acc7f4869c178ad1f73e9e3056b
Eugene Ekaputra <eugeneekaputra@gmail.com>
2022-05-01 10:51:08 +0700

    Add delete modal

--- src/pages/Files.js
+++ src/pages/Files.js
@@ -18,6 +18,7 @@ class Files extends Component {
     this.state = {
       showModal: false,
       selectedId: 0,
+      event: null,
     };
   }
 
@@ -108,8 +109,14 @@ class Files extends Component {
                                   {this.props.account ===
                                     file.uploader.toLowerCase() && (
                                     <button
-                                      onClick={(e) =>
-                                        this.props.deleteFile(file.id, e)
+                                      onClick={
+                                        () =>
+                                          this.setState({
+                                            showDeleteModal: true,
+                                            selectedId: file.id,
+                                          })
+                                        // (e) =>
+                                        // this.props.deleteFile(file.id, e)
                                       }
                                       className="bg-red-400 text-white rounded-xl p-2"
                                     >
@@ -210,6 +217,56 @@ class Files extends Component {
                       form="form1"
                     >
                       Save Changes
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
+
+        {this.state.showDeleteModal ? (
+          <>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="relative w-1/2 my-6 mx-auto max-w-3xl">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                    <h3 className="text-3xl font-semibold">Delete File</h3>
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      onClick={() => this.setState({ showModal: false })}
+                    >
+                      <span className="bg-transparent text-red-600 opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        x
+                      </span>
+                    </button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative p-6 flex-auto">
+                    <h5 className="text-xl">
+                      Are you sure you want to delete file?
+                    </h5>
+                  </div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                    <button
+                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => this.setState({ showModal: false })}
+                    >
+                      No
+                    </button>
+                    <button
+                      className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      onClick={(e) => {
+                        this.props.deleteFile(this.state.selectedId, e);
+                        this.setState({ showModal: false, selectedId: 0 });
+                      }}
+                    >
+                      Yes
                     </button>
                   </div>
                 </div>
